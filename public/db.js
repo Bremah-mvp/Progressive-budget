848d1d691ef22e490a3bf906e45ac8906ffc2b61
jackie <mvpbremah@yahoo.com>
2020-10-22 22:57:10 -0400

    commit

--- public/db.js
+++ public/db.js
@@ -37,5 +37,39 @@ function saveRecord(record) {
 function checkDatabase() {
     // open transaction on your pending database
     const transaction = db.transaction(['pending'], 'readwrite');
-    
+    // access your pending object store
+    const store = transaction.objectStore('pending');
+    // get all records from store and set to a variable
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    "content-Type" : "application/json",
+
+                },
+            })
+
+            .then(response => response.json())
+            .then(() => {
+                // open a transaction on your pending database
+                const transaction = db.transaction(['pending'], 'readwrite');
+
+                // access your pending object store
+                const store = transaction.objectStore('pending');
+
+                // clear all items in your store
+                store.clear();
+
+            });
+        }
+    };
+
 }
+
+// applicaction coming back online
+window.addEventListener('online', checkDatabase);
