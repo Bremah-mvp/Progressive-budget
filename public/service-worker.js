1e447dc52ca4989bd1994119d82bb7113fbfe5a7
jackie <mvpbremah@yahoo.com>
2020-10-24 20:42:21 -0400

    commit

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,13 +12,13 @@ const FILES_TO_CACHE = [
 
   ];
   
-  const PRECACHE = 'static-cache-v2';
-  const RUNTIME = 'data-cache-v1';
+  const CACHE_NAME = 'static-cache-v2';
+  const DATA_CACHE_NAME = 'data-cache-v1';
   
   // Install and add service worker
   self.addEventListener('install', function(evt) {
     evt.waitUntil(
-      caches.open(PRECACHE).then(cache => {
+      caches.open(CACHE_NAME).then(cache => {
         console.log('Your files were pre-cached successfully!');
         return cache.addAll(FILES_TO_CACHE);
       })
