38a81e1d2e967657e19f2d04cab992f9c5a07e6f
jackie <mvpbremah@yahoo.com>
2020-10-24 22:12:54 -0400

    commit

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,65 +1,82 @@
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
-  '/assets/css/styles.css',
-  '/assets/js/index.js',
-  '/assets/js/db.js',
-  '/assets/icons/icon-192x192.png',
-  '/assets/icons/icon-512x512.png',
+  '/styles.css',
+  '/index.js',
   '/manifest.webmanifest',
-  'https:stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
-  'https:cdn.jsdelivr.net/npm/chart.js@2.8.0'
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+  '/db.js',
 ];
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
-//install
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches
-      .open(PRECACHE)
-      .then((cache) => cache.addAll(FILES_TO_CACHE))
-      .then(self.skipWaiting())
+
+const CACHE_NAME = 'static-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
+
+// Install and add service worker
+self.addEventListener('install', function(evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Your files were pre-cached successfully!');
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+
+  self.skipWaiting();
 });
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', (event) => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) => {
-        return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-      })
-      .then((cachesToDelete) => {
-        return Promise.all(
-          cachesToDelete.map((cacheToDelete) => {
-            return caches.delete(cacheToDelete);
-          })
-        );
-      })
-      .then(() => self.clients.claim())
+
+// Activate the service worker and remove old data from the cache
+self.addEventListener('activate', function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log('Removing old cache data', key);
+            return caches.delete(key);
+          }
+        })
+      )
+    )
   );
+
+  self.clients.claim();
 });
-//fetch data
-self.addEventListener('fetch', (event) => {
-  if (event.request.method != 'GET') {
-    event.respondWith(fetch(event.request));
-    return;
-  } else if (event.request.url) {   //help from classmates
-    event.respondWith(
-      caches.open(RUNTIME).then(cache => {
-        return fetch(event.request).then(response => {
-            //if the response is good, clone the response
-            if(response.status === 200) {
-              cache.put(event.request.url, response.clone());
-            }
-            return response;
-          })
-          .catch(err => {
-            return caches.match(event.request);
-          })
-      }).catch(err => (console.log(err)))
+
+// Enable the service worker to intercept network requests
+self.addEventListener('fetch', function(evt) {
+  if (evt.request.url.includes('/api/')) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then(cache =>
+          fetch(evt.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch(err =>
+              // Network request failed, try to get it from the cache.
+              cache.match(evt.request)
+            )
+        )
+        .catch(err => console.log(err))
     );
+
     return;
   }
+  
+  // Allows the page to be accessible offline, shows files from the cache
+  evt.respondWith(
+    caches
+      .open(CACHE_NAME)
+      .then(cache =>
+        cache
+          .match(evt.request)
+          .then(response => response || fetch(evt.request))
+      )
+  );
 });
