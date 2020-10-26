a8bbf36a7ee637a4b69322e987e073a996167e15
jackie <mvpbremah@yahoo.com>
2020-10-25 21:07:22 -0400

    commit

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+ 
+require("dotenv").config();
 
 const PORT = 3000;
 
@@ -15,7 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/progressivebudget", {
+mongoose.connect( process.env.MONGODB_URI||"mongodb://localhost/progressivebudget", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
